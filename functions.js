d2c8d8bf18fe6a34da1fc611a68085371444e4e5
jamclean23 <jamclean23@gmail.com>
2023-01-11 16:28:14 -0500

    Add caesar cipher with case sensitivity and punctuation

--- functions.js
+++ functions.js
@@ -15,24 +15,29 @@ function capitalize (string) {
 }
 
 function reverseString (string) {
-
+    string.toString();
+    let result = string.split('');
+    result = result.reverse();
+    result = result.join('');
+    return result;
 }
 
 const Calculator = ( () => {
 
     function add (firstNumber, secondNumber) {
+        return +firstNumber + +secondNumber;
     }
 
     function subtract (firstNumber, secondNumber) {
-
+        return +firstNumber - +secondNumber;
     }
 
     function divide (firstNumber, secondNumber) {
-
+        return +firstNumber / +secondNumber;
     }
 
     function multiply (firstNumber, secondNumber) {
-
+        return +firstNumber * + secondNumber;
     }
 
     return {
@@ -43,8 +48,62 @@ const Calculator = ( () => {
     }
 })();
 
-function caesarCipher (string) {
+function caesarCipher (string, offset) {
+    return shiftArray(toArray(string), offset);
 
+
+    function toArray (string) {
+        return string.split('');
+    }
+
+    function shiftArray (array, offset) {
+        let result = '';
+        array.forEach((item, index) => {
+            if (/[a-zA-Z]/.test(item)) {
+                result += shiftCharacter(item, offset);
+            } else if (/\d/.test(item)) {
+                result += shiftDigit(item, offset);
+            } else {
+                result += item;
+            }
+        });
+        return result;
+    }
+
+    function shiftCharacter (item, offset) {
+        let characters = 'abcdefghijklmnopqrstuvwxyz';
+        characters = characters.split('');
+        let result = '';
+        
+        characters.forEach((character, index) => {
+            if (character === item.toLowerCase()) {
+                let adjustedOffset = index + offset;
+                if (adjustedOffset > 25) {
+                    adjustedOffset -= 26;
+                } else if (adjustedOffset < 0) {
+                    adjustedOffset += 26;
+                }
+
+                result = characters[adjustedOffset];
+
+                if (/[A-Z]/.test(item)) {
+                    result = result.toUpperCase();
+                }
+            }
+        });
+        return result;
+    }
+
+    function shiftDigit (stringDigit, offset) {
+        let result = +stringDigit + offset;
+        if (result > 9) {
+            return result -= 10;
+        } else if (result < 0) {
+            return result += 10;
+        } else {
+            return result;
+        }
+    }
 }
 
 
--- functions.test.js
+++ functions.test.js
@@ -41,4 +41,64 @@ test('Capitalize converts values to strings and returns them capitalized',  () =
 });
 
 
-// 
+// REVERSE STRING
+
+test('ReverseString reverses strings', () => {
+    expect(Practice.reverseString('blah')).toBe('halb');
+    expect(Practice.reverseString('howdy do')).toBe('od ydwoh');
+    expect(Practice.reverseString('lala 123')).toBe('321 alal');
+});
+
+// CALCULATOR
+
+// ADD
+
+test('Adds numbers', () => {
+    expect(Practice.Calculator.add(2, 2)).toBe(4);
+    expect(Practice.Calculator.add(-2, -2)).toBe(-4);
+    expect(Practice.Calculator.add(0, 0)).toBe(0);
+    expect(Practice.Calculator.add(-2, 2)).toBe(0);
+});
+
+// SUBTRACT
+
+test('Subtracts numbers', () => {
+    expect(Practice.Calculator.subtract(2, 2)).toBe(0);
+    expect(Practice.Calculator.subtract(-2, -2)).toBe(0);
+    expect(Practice.Calculator.subtract(0, 0)).toBe(0);
+    expect(Practice.Calculator.subtract(-2, 2)).toBe(-4);
+});
+
+// DIVIDE
+
+test('Divides numbers', () => {
+    expect(Practice.Calculator.divide(2, 2)).toBe(1);
+    expect(Practice.Calculator.divide(144, 12)).toBe(12);
+    expect(Practice.Calculator.divide(144, -12)).toBe(-12);
+    expect(Practice.Calculator.divide(2, 0)).toBe(Infinity);
+    expect(Practice.Calculator.divide(0, 2)).toBe(0);
+});
+
+// MULTIPLY
+
+test('Multiplies numbers',() => {
+    expect(Practice.Calculator.multiply(2, 2)).toBe(4);
+    expect(Practice.Calculator.multiply(2, 3)).toBe(6);
+    expect(Practice.Calculator.multiply(2, -2)).toBe(-4);
+    expect(Practice.Calculator.multiply(2, -3)).toBe(-6);
+    expect(Practice.Calculator.multiply(0, 0)).toBe(0);
+});
+
+// CAESAR CIPHER
+
+test('Converts a string by the designated offset with a caesar cipher', () => {
+    expect(Practice.caesarCipher('Defend2', 1)).toBe('Efgfoe3');
+    expect(Practice.caesarCipher('defend', -1)).toBe('cdedmc');
+    expect(Practice.caesarCipher('z', 1)).toBe('a');
+    expect(Practice.caesarCipher('a', -1)).toBe('z');
+    expect(Practice.caesarCipher('78...9', 1)).toBe('89...0');
+    expect(Practice.caesarCipher('1', -8)).toBe('3');
+});
+
+// ANALYZE ARRAY
+
